c48e5c10d55a1bd7f0143f33bdf0859fd87b729d
yumemi-ysakai <y_sakai@yumemi.co.jp>
2019-08-22 12:06:21 +0900

    refactor

--- main.go
+++ main.go
@@ -5,20 +5,24 @@ import (
     "github.com/labstack/echo"
 )
 
+func sample(c echo.Context) error {
+    return c.JSON(
+        http.StatusOK,
+        struct {
+            Code int    `json:"code"`
+            Text string `json:"body"`
+        }{
+            Code: http.StatusOK,
+            Text: http.StatusText(http.StatusOK),
+        },
+    )
+}
+
 func main() {
     e := echo.New()
-    e.GET("/sample", func(c echo.Context) error {
-        return c.JSON(
-            http.StatusOK,
-            struct {
-                Code int    `json:"code"`
-                Text string `json:"body"`
-            }{
-                Code: http.StatusOK,
-                Text: http.StatusText(http.StatusOK),
-            },
-        )
-    })
+    // Routes
+    e.GET("/sample", sample)
+    // Start server
     e.Logger.Fatal(e.Start(":8080"))
 }
 
